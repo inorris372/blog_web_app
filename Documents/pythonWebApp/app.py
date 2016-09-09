176939247d29b3430364ea84d8f74fabda8515b8
Ian Norris <inorris372@gmail.com>
2016-09-09 13:20:54 -0500

    Implementing Mongodb

--- Documents/pythonWebApp/app.py
+++ Documents/pythonWebApp/app.py
@@ -2,4 +2,14 @@ __author__ = 'Ian'
 
 import pymongo
 
+uri = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(uri)
+database = client['fullstack']
+collection = database['students']
 
+students = collection.find({})
+
+print(students)
+
+for student in students:
+    print(student)
